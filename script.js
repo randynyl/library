b53574f52fb400505f0134c2d0b1d9ad59b68f93
Randy Ng <randy.ng@hotmail.sg>
2021-07-31 19:12:23 +0800

    Add form layout

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ function addBookToLibrary() {
 
 function displayBooks(bookArr) {
     const bookContainer = document.getElementById('book-container');
+
     bookArr.forEach(book => {
         let bookCard = document.createElement('div');
         let titleText = document.createElement('div');
@@ -35,19 +36,61 @@ function displayBooks(bookArr) {
         titleText.classList.add('book-card-text');
         authorText.classList.add('book-card-text');
         pageNum.classList.add('book-card-text');
-        
+
         console.log(book.title);
         titleText.innerHTML = book.title;
         authorText.innerHTML = book.author;
-        pageNum.innerHTML = book.pages;
+        pageNum.innerHTML = book.pages + ' pages';
 
         bookCard.appendChild(titleText);
         bookCard.appendChild(authorText);
         bookCard.appendChild(pageNum);
         bookCard.classList.add('book-card');
+
         bookContainer.appendChild(bookCard);
+    })
+    // display 'Add book' card
+    let addBookCard = document.createElement('div');
+    let addBtn = document.createElement('button');
+
+    addBtn.innerHTML = 'ADD BOOK';
+    addBtn.classList.add('add-btn');
+    addBookCard.appendChild(addBtn);
+    addBookCard.classList.add('book-card');
+    bookContainer.appendChild(addBookCard);
+    addBtn.addEventListener('click', () => {
+        const form = document.querySelector('#add-book-form');
+        openForm(form);
+    })
+
+}
+
+function openForm(form) {
+    if (form == null) return;
+
+    const overlay = document.getElementById('overlay');
+    const closeFormBtns = document.querySelectorAll('[data-close-button]');
+
+    form.classList.add('active');
+    overlay.classList.add('active');
+    
+    closeFormBtns.forEach(button => {
+        button.addEventListener('click', () => {
+            const form = button.closest('.form');
+            closeForm(form);
+        })
+    })
+    overlay.addEventListener('click', () => {
+        const form = document.querySelector('.form.active')
+        closeForm(form);
     })
 }
 
+
+function closeForm(form) {
+    if (form == null) return;
+    form.classList.remove('active');
+    overlay.classList.remove('active');
+}
 addBookToLibrary();
 displayBooks(myLibrary);
